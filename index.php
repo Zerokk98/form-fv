1f4ee74258611233a28c36afc968caef446a6d91
Zerokk98 <133662459+Zerokk98@users.noreply.github.com>
2023-09-11 07:37:10 -0500

    index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-
 $connect = mysqli_connect('localhost', 'usb', 'usb2022', 'formulatio');
 
 $email = isset($_POST['email']) ? $_POST['email'] : '';
 $message = isset($_POST['message']) ? $_POST['message'] : '';
+$cliente = isset($_POST['cliente']) ? $_POST['cliente'] : '';
+$departamento = isset($_POST['department']) ? $_POST['department'] : '';
 
 $email_error = '';
 $message_error = '';
 
-// Crear tres arreglos con nombres de empleados para cada departamento
 $atencionCliente = ['Empleado1', 'Empleado2', 'Empleado3', 'Empleado4'];
 $soporteTecnico = ['EmpleadoA', 'EmpleadoB', 'EmpleadoC', 'EmpleadoD'];
 $facturacion = ['EmpleadoX', 'EmpleadoY', 'EmpleadoZ', 'EmpleadoW'];
@@ -16,102 +16,84 @@ $facturacion = ['EmpleadoX', 'EmpleadoY', 'EmpleadoZ', 'EmpleadoW'];
 if (count($_POST)) {
     $errors = 0;
 
-    if ($_POST['email'] == '') {
+    if ($email == '') {
         $email_error = 'Please enter an email address';
         $errors++;
     }
 
-    if ($_POST['message'] == '') {
+    if ($message == '') {
         $message_error = 'Please enter a message';
         $errors++;
     }
 
     if ($errors == 0) {
-        // Obtener un nombre aleatorio de acuerdo al departamento seleccionado
-        $department = $_POST['department'];
-        $randomName = '';
+        $empleadoSeleccionado = '';
 
-        switch ($department) {
+        switch ($departamento) {
             case 'atencion_cliente':
-                $randomName = $atencionCliente[array_rand($atencionCliente)];
+                $empleadoSeleccionado = $atencionCliente[array_rand($atencionCliente)];
                 break;
             case 'soporte_tecnico':
-                $randomName = $soporteTecnico[array_rand($soporteTecnico)];
+                $empleadoSeleccionado = $soporteTecnico[array_rand($soporteTecnico)];
                 break;
             case 'facturacion':
-                $randomName = $facturacion[array_rand($facturacion)];
+                $empleadoSeleccionado = $facturacion[array_rand($facturacion)];
                 break;
         }
 
         $query = 'INSERT INTO contact (
                 email,
                 message,
-                department
+                department,
+                employee_name,
+                cliente
             ) VALUES (
-                "' . addslashes($_POST['email']) . '",
-                "' . addslashes($_POST['message']) . '",
-                "' . $randomName . '"
+                "' . addslashes($email) . '",
+                "' . addslashes($message) . '",
+                "' . $departamento . '",
+                "' . $empleadoSeleccionado . '",
+                "' . $cliente . '"
             )';
         mysqli_query($connect, $query);
 
-        $message = 'You have received a contact form submission:
-            
-Email: ' . $_POST['email'] . '
-Message: ' . $_POST['message'] . '
-Department: ' . $randomName;
+        $mensajeCorreo = 'You have received a contact form submission:
 
-        mail('poveda.geovanny@hotmail.com',
-            'Contact Form Submission',
-            $message);
+Email: ' . $email . '
+Message: ' . $message . '
+Department: ' . $departamento . '
+Employee Name: ' . $empleadoSeleccionado . '
+Cliente: ' . $cliente;
+
+        mail('poveda.geovanny@hotmail.com', 'Contact Form Submission', $mensajeCorreo);
 
         header('Location: thankyou.html');
         die();
     }
 }
-
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html>
-
 <head>
     <title>PHP Contact Form</title>
 </head>
-
 <body>
-
     <h1>PHP Contact Form</h1>
-
     <form method="post" action="">
-
-        Email Address:
-        <br>
+        Email Address:<br>
         <input type="text" name="email" value="<?php echo $email; ?>">
-        <?php echo $email_error; ?>
-
-        <br><br>
-
-        Message:
-        <br>
+        <?php echo $email_error; ?><br><br>
+        Message:<br>
         <textarea name="message"><?php echo $message; ?></textarea>
-        <?php echo $message_error; ?>
-
-        <br><br>
-
-        <!-- Agregar un campo para seleccionar el departamento -->
-        Department:
-        <br>
+        <?php echo $message_error; ?><br><br>
+        Name of Client:<br>
+        <input type="text" name="cliente" value="<?php echo $cliente; ?>"><br><br>
+        Department:<br>
         <select name="department">
             <option value="atencion_cliente">Atención al Cliente</option>
             <option value="soporte_tecnico">Soporte Técnico</option>
             <option value="facturacion">Facturación</option>
-        </select>
-
-        <br><br>
-
+        </select><br><br>
         <input type="submit" value="Submit">
-
     </form>
-
 </body>
-
 </html>
